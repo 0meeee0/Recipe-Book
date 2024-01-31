537327299621e2501200041c52d03a6a775e3b2c
0meeee0 <mehdi74.id@gmail.com>
2024-01-31 02:08:00 +0100

    lookin good

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,19 @@
+
+  <!-- bootstrap core css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.css') }}" />
+
+<!-- fonts style -->
+<link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet">
+
+<!-- font awesome style -->
+<link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" />
+<!-- nice select -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" integrity="sha256-mLBIhmBvigTFWPSCtvdu6a76T+3Xyt+K571hupeFLg4=" crossorigin="anonymous" />
+<!-- slidck slider -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css" integrity="sha256-UK1EiopXIL+KVhfbFa8xrmAWPeBjMVdvYMYkTAEv/HI=" crossorigin="anonymous" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css.map" integrity="undefined" crossorigin="anonymous" />
+
+<!-- Custom styles for this template -->
+<link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
+<!-- responsive style -->
+<link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet" />
---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,10 @@
+<!-- jQuery -->
+<script src="{{ asset('resources/js/jquery-3.4.1.min.js') }}"></script>
+<!-- Bootstrap JS -->
+<script src="{{ asset('resources/js/bootstrap.js') }}"></script>
+<!-- Slick Slider -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.js"></script>
+<!-- Nice Select -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js" integrity="sha256-Zr3vByTlMGQhvMfgkQ5BtWRSKBGa2QlspKYJnkjZTmo=" crossorigin="anonymous"></script>
+<!-- Custom JS -->
+<script src="{{ asset('resources/js/custom.js') }}"></script>
